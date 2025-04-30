2047ba0003ec669a1ae9c6e7b8c5c12298ceb936
Anas Rashid <125889610+anasrashid-1@users.noreply.github.com>
2025-04-30 08:37:49 +0530

    Add initial implementation of models and embeddings with example queries

---
+++ 1. Models/2.ChatModels/4_chatmodel_huggingface_api.py
@@ -0,0 +1,13 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatHuggingFace(llm = HuggingFaceEndpoint(
+    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation"
+))
+
+result = model.invoke("What is the capital of India")
+
+print(result.content)
